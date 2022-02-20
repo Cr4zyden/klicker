774018cbb162b039b90fb346b54b783ed584cc68
Cr4zyden <90123052+Cr4zyden@users.noreply.github.com>
2022-02-20 10:30:58 +0700

    Delete project directory

--- project/interface_buttons/name.py
+++
@@ -1,8 +0,0 @@
-import pygame.image
-
-class Name:
-    def __init__(self):
-        self.image = pygame.image.load('img/name.png')
-        self.pos = (100,0)
-    def draw(self,screen):
-        screen.blit(self.image, self.pos)
